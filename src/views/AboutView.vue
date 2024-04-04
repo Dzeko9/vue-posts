d323177c4ece980659dbe8b0d8c4b71db253e5a5
HWAN`E <77482552+Dzeko9@users.noreply.github.com>
2024-04-04 12:23:42 +0900

    Vue3 게시판 프로젝트

---
+++ src/views/AboutView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2 class="mt-4">About View</h2>
+    <button class="btn btn-primary" @click="$router.push('/')">Home으로 이동</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
